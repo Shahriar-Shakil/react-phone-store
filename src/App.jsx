a1b76463805afc19935dc44cd4bfb1a0928bea68
Shahriar-Shakil <toshahriarshakil@gmail.com>
2019-05-02 19:21:00 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import './App.css';
+import {Switch,Route} from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Navbar from './components/Navbar'
+import ProductList from './components/ProductList'
+import Details from './components/Details'
+import Cart from './components/cart/Cart'
+import Default from './components/Default'
+import Modal from './components/Modal'
+
+class App extends React.Component {
+  state = {  }
+  render() { 
+    return ( 
+      <>
+      <Navbar />
+      <Switch>
+        <Route exact path="/" component={ProductList} />
+          <Route path="/Details" component={Details} />
+          <Route path="/Cart" component={Cart} />
+          <Route component={Default} />
+        </Switch>
+        <Modal />
+        
+      </>
+     );
+  }
+}
+ 
+export default App;
+
+
